ab7956b85eac10bcf59e130580e60a90345843c1
muratcancicek <muratcancicek0@gmail.com>
2018-11-19 02:04:54 -0800

    Visual Studio 2017 Python 3 Project has been created for future work.

---
+++ DeepRL_For_HPE/Core/run.py
@@ -0,0 +1,7 @@
+# Author: Muratcan Cicek, https://users.soe.ucsc.edu/~cicekm/
+
+def main():
+    print('This Project will be training a set of Deep Reinforcement Algorithms for Head Pose Estimation.')
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# DeepRL_For_HeadPoseEst
+# Deep Reinforcement Learning forHead Pose Estimation
 
 This Repository will be containing a set of Deep Reinforcement Algorithms for Head Pose Estimation
