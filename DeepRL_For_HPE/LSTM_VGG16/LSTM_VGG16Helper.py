922a89b14c00fa6167c7ca45b03d9c36eb21eb50
muratcancicek <muratcancicek0@gmail.com>
2018-12-31 00:25:41 -0800

    Implementing LSTM_VGG16Helper

---
+++ DeepRL_For_HPE/LSTM_VGG16/LSTM_VGG16Helper.py
@@ -0,0 +1,58 @@
+# Author: Muratcan Cicek, https://users.soe.ucsc.edu/~cicekm/
+import os
+#Dirty importing that allows the main author to switch environments easily
+if '.' in __name__:
+    from Core.NeighborFolderimporter import *
+else:
+    from NeighborFolderimporter import *
+
+from DatasetHandler.BiwiBrowser import *
+
+import keras
+import numpy as np
+#from keras import Model 
+from keras.layers import *
+import matplotlib.pyplot as plt
+from keras.optimizers import SGD
+from keras.models import Sequential
+from keras.constraints import maxnorm
+from keras.applications.vgg16 import VGG16
+from sklearn.preprocessing import MinMaxScaler
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.convolutional import Convolution2D
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+from keras.preprocessing.sequence import TimeseriesGenerator
+
+def trainOnSets(model, set_gen, timesteps, output_begin, num_outputs, batch_size, epochs = 1):
+    c = 0
+    for inputMatrix, labels in set_gen:
+        print('%d. dataset being trained!' % (c+1))
+        labels = labels[:, output_begin:output_begin+num_outputs]
+        data_gen = TimeseriesGenerator(inputMatrix, labels, length=timesteps, batch_size=batch_size)
+        model.fit_generator(data_gen, steps_per_epoch=len(data_gen), epochs=epochs, verbose=1) 
+        c += 1
+    return model
+
+def trainForEpochs(model, epochs, subjectList, testSubjects, timesteps, output_begin, num_outputs, batch_size, in_epochs = 1):
+    trainingSubjects = [s for s in subjectList if s not in testSubjects]
+    for e in range(epochs):
+        trainingBiwi = readBIWIDataset(subjectList = trainingSubjects) 
+        model = trainOnSets(model, trainingBiwi, timesteps, output_begin, num_outputs, batch_size, in_epochs)
+        print('Epoch %d completed!' % (e+1))
+    return model
+
+def getTestBiwi(testSubjects, timesteps, output_begin, num_outputs, batch_size):
+    test_generators, test_labelSets = [], [] 
+    testBiwi = readBIWIDataset(subjectList = testSubjects) 
+    for inputMatrix, labels in testBiwi:
+        labels = labels[:, output_begin:output_begin+num_outputs]
+        data_gen = TimeseriesGenerator(inputMatrix, labels, length=timesteps, batch_size=batch_size)
+        test_generators.append(data_gen)
+        test_labelSets.append(labels[:, 4:5])
+    return test_generators, test_labelSets
+
+if __name__ == "__main__":
+    print('Done')
