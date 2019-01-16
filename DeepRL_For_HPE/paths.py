a80d39e28a151fb1f372771ffd060df1386eacc4
muratcancicek <muratcancicek0@gmail.com>
2019-01-16 00:47:02 -0800

    Default models folder

--- DeepRL_For_HPE/paths.py
+++ DeepRL_For_HPE/paths.py
@@ -4,5 +4,7 @@ import os
 
 if 'COMPUTERNAME' in os.environ:
     BIWI_Main_Folder = "C:/cStorage/Datasets/hpdb/"
+    Keras_Models_Folder = "C:/cStorage/Datasets/Keras_Models/"
 else:
     BIWI_Main_Folder = "/home/mcicek/Datasets/HeadPoses/biwi/"
+    Keras_Models_Folder = "/home/mcicek/Datasets/Keras_Models/"
