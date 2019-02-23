d09706c4653aaa86243cdadff0f6b0595ad27162
muratcancicek <muratcancicek0@gmail.com>
2019-02-22 22:46:19 -0800

    Replacing tf with K

--- DeepRL_For_HPE/FC_RNN_Evaluater/ReinforceAlgorithmForKerasModels.py
+++ DeepRL_For_HPE/FC_RNN_Evaluater/ReinforceAlgorithmForKerasModels.py
@@ -7,7 +7,7 @@ else:
     from EvaluationRecorder import printProgressBar
 
 from keras.initializers import RandomNormal
-from keras import backend as k
+from keras import backend as K
 from keras import losses
 import tensorflow as tf
 import numpy as np
@@ -21,20 +21,20 @@ def drawSamples(model, episodes, sigma, outputs, seed=None):
     
 def getRewardsWithBaselinePerEpisode(samples, targets):
     duplicated_targets = np.repeat(targets[np.newaxis, ...], repeats = samples.shape[0], axis=0)
-    duplicated_targets = tf.convert_to_tensor(duplicated_targets, name='targets', dtype=tf.float32)
-    abs_difference = tf.abs(samples - duplicated_targets, name='abs_difference')
-    allRewards = - tf.reduce_mean(abs_difference, axis = -1) - tf.reduce_mean(abs_difference, axis = -1)
-    rewardsPerEpisode = tf.reduce_sum(allRewards, axis = -1, name='rewardsPerEpisode')
-    baseline = tf.reduce_mean(allRewards, axis = 0, name='baseline')
+    duplicated_targets = K.convert_to_tensor(duplicated_targets, name='targets', dtype=K.float32)
+    abs_difference = K.abs(samples - duplicated_targets, name='abs_difference')
+    allRewards = - K.reduce_mean(abs_difference, axis = -1) - K.reduce_mean(abs_difference, axis = -1)
+    rewardsPerEpisode = K.reduce_sum(allRewards, axis = -1, name='rewardsPerEpisode')
+    baseline = K.reduce_mean(allRewards, axis = 0, name='baseline')
     allRewardsWithBaseline = allRewards - baseline
-    return tf.reduce_sum(allRewardsWithBaseline, axis=-1, name='rewardsWithBaselinePerEpisode')
+    return K.reduce_sum(allRewardsWithBaseline, axis=-1, name='rewardsWithBaselinePerEpisode')
     
 def getGradientsPerEpisode(model, samples, targets):
     gradients_per_episode = []
     rewards = getRewardsWithBaselinePerEpisode(samples, targets)
     for i in range(samples.shape[0]):
         loss = losses.mean_absolute_error(targets, samples[i])
-        gradients = k.gradients(loss, model.trainable_weights)
+        gradients = K.gradients(loss, model.trainable_weights)
         rewardedGradients = [g*rewards[i] for g in gradients]
         gradients_per_episode.append(rewardedGradients)
     return gradients_per_episode
@@ -43,17 +43,17 @@ def getFinalGradients(model, samples, targets):
     gradients_per_episode = getGradientsPerEpisode(model, samples, targets)
     stacked_gradients = []
     for i in range(len(gradients_per_episode[0])):
-        stacked_gradients.append(tf.stack([gradients[i] for gradients in gradients_per_episode])) 
-    return [tf.reduce_mean(g, axis=0) for g in stacked_gradients]
+        stacked_gradients.append(K.stack([gradients[i] for gradients in gradients_per_episode])) 
+    return [K.reduce_mean(g, axis=0) for g in stacked_gradients]
 
 def getUpdatedModelWithGradients(model, gradients):
     for i in range(len(model.trainable_weights)):
-        tf.assign_sub(model.trainable_weights[i], gradients[i])
+        K.assign_sub(model.trainable_weights[i], gradients[i])
     return model
 
 def getOutputTensor(model, inputMatrix_batch, inputLabels_batch):
-    im = tf.convert_to_tensor(inputMatrix_batch, name='im', dtype=tf.float32)
-    il = tf.convert_to_tensor(inputLabels_batch, name='il', dtype=tf.float32)
+    im = K.convert_to_tensor(inputMatrix_batch, name='im', dtype=K.float32)
+    il = K.convert_to_tensor(inputLabels_batch, name='il', dtype=K.float32)
     return model([im, il])
 
 def reinforceModelForEpoch(model, data_gen, episodes, sigma, steps_per_epoch, epochCount, verbose=1):
